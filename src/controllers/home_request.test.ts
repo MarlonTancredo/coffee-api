293224dc47bf8ddc1a7910aa5819a6926197d6ef
Marlon Tancredo <marlon_tancredo@hotmail.com>
2023-08-20 17:08:30 +0100

    feat: Implement GET /coffeelover request

--- src/controllers/home_request.test.ts
+++ src/controllers/home_request.test.ts
@@ -5,6 +5,14 @@ describe("Test home API endpoint request", () => {
   test("GET should return correct message", async () => {
     const res = await request(app).get("/");
     expect(res.statusCode).toEqual(200);
-    expect(res.text).toEqual("This Should fail!");
+    expect(res.text).toEqual("Welcome to the Drinks API!");
+  });
+});
+
+describe("Test home API endpoint request", () => {
+  test("GET /coffeelover should return correct message", async () => {
+    const res = await request(app).get("/coffeelover");
+    expect(res.statusCode).toEqual(200);
+    expect(res.text).toEqual("I like coffee!");
   });
 });
