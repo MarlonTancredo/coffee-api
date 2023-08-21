8887f435cd8aad4c0083686ccf7c99f9aad71202
Marlon Tancredo <marlon_tancredo@hotmail.com>
2023-08-21 21:50:04 +0100

    feat: implement tea_controller.test.ts

---
+++ src/controllers/tea_controller.test.ts
@@ -0,0 +1,13 @@
+import request from "supertest";
+import { app } from "../app";
+
+test("GET /tea should return correct object", async () => {
+  const res = await request(app)
+    .get("/tea")
+    .query({ teaName: "English breakfast" });
+  expect(res.statusCode).toEqual(200);
+  expect(res.body).toEqual({
+    drinkType: "Tea",
+    name: "English breakfast",
+  });
+});
